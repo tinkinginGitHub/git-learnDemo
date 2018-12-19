015515e40d6c71c63fe47a3a364337509e549e16
daiping <891130271@qq.com>
2018-12-19 11:38:19 +0800

    changed the class name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-public class Hiworld {
+public class Helloworld {
     public static void main(String[] args) {
          System.out.println("hello world");
          System.err.println("hello world");
