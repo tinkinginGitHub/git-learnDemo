68250ee7a22aa02b2eef066829f429ac193bfa0f
daiping <891130271@qq.com>
2018-12-19 10:37:21 +0800

    change the class name

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,9 @@
-public class Helloworld {
+public class Hiworld {
     public static void main(String[] args) {
          System.out.println("hello world");
          System.err.println("hello world");
+         if(true){
+           System.out.println("add some more code");
+      }
   }
 }
