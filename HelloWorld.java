66f88d197d053b899fdc3d4d90527714e92142a1
daiping <891130271@qq.com>
2018-12-19 10:40:07 +0800

    added return

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class Helloworld {
     public static void main(String[] args) {
          System.out.println("hello world");
          System.err.println("hello world");
+         return;
   }
 }
