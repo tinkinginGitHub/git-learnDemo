dba9369a50370c93500f29b47eb8d399aed1773e
daiping <891130271@qq.com>
2018-12-19 12:06:18 +0800

    reverted to old class name

--- ruby.java
+++ ruby.java
@@ -1,4 +1,4 @@
-public class Helloworld {
+public class Hiworld {
     public static void main(String[] args) {
          System.out.println("hello world");
          System.err.println("hello world");
